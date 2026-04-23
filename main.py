f52d2342b18ba9965f8209a121faaf07ac4d5a12
KhangICO <107622734+KhangICO@users.noreply.github.com>
2026-04-23 09:44:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,21 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 import random, json, os, asyncio, datetime
+from flask import Flask
+from threading import Thread
 
+app = Flask("")
+
+@app.route("/")
+def home():
+    return "Bot is running"
+
+def run():
+    app.run(host="0.0.0.0", port=10000)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
 intents = discord.Intents.default()
 intents.message_content = True
 
